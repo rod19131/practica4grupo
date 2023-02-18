d78618cc851181f96a7d50971ade68d321f67d63
José Alejandro Rodríguez Porras <rod19131@uvg.edu.gt>
2023-02-18 13:44:29 -0600

    clase de jugador

---
+++ game_GUI_Player.py
@@ -0,0 +1,23 @@
+# Clase del jugador
+class Player:
+    def _init_(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.width = 20
+        self.height = 20
+        self.color = color
+
+    def move_left(self):
+        self.x -= 15
+
+    def move_right(self):
+        self.x += 15
+
+    def move_up(self):
+        self.y -= 15
+
+    def move_down(self):
+        self.y += 15
+
+    def draw(self, canvas):
+        canvas.create_rectangle(self.x, self.y, self.x + self.width, self.y + self.height, fill=self.color)
